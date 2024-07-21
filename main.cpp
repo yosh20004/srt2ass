dfd58344befd27365c05ce745cf7ee3e2db8001d
yosh20004 <123@123.com>
2024-07-21 20:32:22 +0800

    基本功能完成

--- main.cpp
+++ main.cpp
@@ -6,13 +6,49 @@
  ************************************************************************/
 
 #include "SRTReader.h"
+#include "srtTransfer.h"
+#include "ASSWriter.h"
+#include <ctime>
 
-int main() {
-    std::string path =
-        ".\\[zmk.pw]Koe.no.katachi.2016.1080p.BluRay.x264.DTS-HDChina.srt";
+std::string path;
+std::string Tpath;
+
+auto analyse(int argc, char* argv[]) -> const bool {
+    if (argc == 1) {
+        std::cout << "necessary message needed";
+        return false;
+    } else if (argc == 2) {
+        path = argv[1];
+        if (path.ends_with(".srt")) {
+            Tpath = std::string(argv[1]);
+            Tpath.erase(Tpath.length() - 3);
+            Tpath += "ass";
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        path = argv[1];
+        Tpath = argv[2];
+        return true;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (!analyse(argc, argv)) {
+        return -1;
+    }
+
     auto a = SRTReader(path);
     a.readLine();
+    //按行读取原文件
 
+    auto sT = srtTransfer(a.getEntry());
+    //解析信息 进行转换
+
+    auto aW = ASSWriter(Tpath, sT.getEntry());
+    aW.write();
+    //写入ass文件
 
 }
 
